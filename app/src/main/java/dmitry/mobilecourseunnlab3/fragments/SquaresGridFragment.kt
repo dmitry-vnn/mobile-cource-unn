3cd5889d95de1e40694ba2a3802b50f2452a7400
Dmitry <xtopz1337@gmail.com>
2024-03-27 17:49:50 +0300

    change: force commit

---
+++ app/src/main/java/dmitry/mobilecourseunnlab3/fragments/SquaresGridFragment.kt
@@ -0,0 +1,20 @@
+package dmitry.mobilecourseunnlab3.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dmitry.mobilecourseunnlab3.R
+
+class SquaresGridFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_squares_grid, container, false)
+    }
+
+}

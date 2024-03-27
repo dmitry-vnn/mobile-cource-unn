f961b119d50d62c8adc69212ffbbbf13695a2dac
Dmitry <xtopz1337@gmail.com>
2024-03-27 17:50:58 +0300

    refactor: remove unused imports

--- app/src/main/java/dmitry/mobilecourseunnlab3/SquaresActivity.kt
+++ app/src/main/java/dmitry/mobilecourseunnlab3/SquaresActivity.kt
@@ -1,10 +1,7 @@
 package dmitry.mobilecourseunnlab3
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SquaresActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
